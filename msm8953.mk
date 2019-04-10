02a12701546fa266e9a9b360dcae26377446708e
bgcngm <bgcngm@gmail.com>
2019-04-10 17:58:56 +0200

    msm8953-common: Switch to vendor.display.config 1.1
     * No need to explicitly build the packages anymore,
       since they're now display HAL dependencies.
    
    Change-Id: Ied3be4c1e5fc7e01e0426268fb219c61a4be86e9

--- msm8953.mk
+++ msm8953.mk
@@ -147,14 +147,11 @@ PRODUCT_PACKAGES += \
     liboverlay \
     libqdMetaData.system \
     libgenlock \
-    libtinyxml \
-    vendor.display.config@1.0 \
-    vendor.display.config@1.1 \
-    vendor.display.config@1.0_vendor \
-    vendor.display.config@1.1_vendor
-	
-PRODUCT_BOOT_JARS += \
-    WfdCommon
+    libtinyxml
+
+# Doze mode
+PRODUCT_PACKAGES += \
+    XiaomiDoze
 
 # DRM
 PRODUCT_PACKAGES += \
