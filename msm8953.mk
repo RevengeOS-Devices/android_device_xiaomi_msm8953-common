ae01a11d03027fa9ef7e867a82ee563aaa8463cb
Lucchetto <lucchetto.tie@live.com>
2019-05-01 18:29:06 +0000

    msm8953-common: derps

--- msm8953.mk
+++ msm8953.mk
@@ -289,11 +289,6 @@ PRODUCT_COPY_FILES += \
 # QMI
 PRODUCT_PACKAGES += \
     libjson
-	
-#QTI performance
-PRODUCT_BOOT_JARS += \
-    QPerformance \
-    UxPerformance
 
 # Ramdisk
 PRODUCT_PACKAGES += \
